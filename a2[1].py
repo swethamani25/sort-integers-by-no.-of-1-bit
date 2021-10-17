3947d2d3d730cbf78fc447cf1e5864992ecfe065
swethamani25 <92358649+swethamani25@users.noreply.github.com>
2021-10-17 13:36:30 +0530

    Add files via upload

---
+++ a2[1].py
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        return Arrays.stream(arr).boxed()
+        .sorted(Comparator.comparingInt(Integer::bitCount).thenComparingInt(o -> o))
+        .collect(Collectors.toList())
+        .stream()
+        .mapToInt(Integer::intValue)
+        .toArray();
+  }
+}
